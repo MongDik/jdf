a88f4bcf49c02362aa20f38dd46ad07a1107852e
孟迪 <mengdi@sf-express.com>
2014-09-02 18:41:46 +0800

    update

---
+++ jdf-vendor/src/main/java/com/mong/jdf/jdf_vendor/App.java
@@ -0,0 +1,13 @@
+package com.mong.jdf.jdf_vendor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
